a6529614e81e00aadbee12636f29353a6b92864d
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-11-30 22:15:28 +0800

    Update test_question_1.py 1

--- test_question_1.py
+++ test_question_1.py
@@ -5,8 +5,8 @@ from question_1 import calculation
 def test_calculation():
     # Test case 1
     result = calculation(40, 10)
-    assert result == (50, 30), "Test case 1 failed"
+    assert result == (400, 4), "Test case 1 failed"
 
     # Test case 2 (add more as needed)
     result = calculation(20, 5)
-    assert result == (25, 15), "Test case 2 failed"
+    assert result == (100, 4), "Test case 2 failed"
