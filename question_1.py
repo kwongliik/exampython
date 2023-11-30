ad58552861912794e8a488b4998c212ad690dbb3
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-11-30 22:14:21 +0800

    Update question_1.py 2

--- question_1.py
+++ question_1.py
@@ -1,6 +1,6 @@
 def calculation(a, b):
-    addition = a ....... b
-    subtraction = a ...... b
+    multiplication = a ....... b
+    division = a ...... b
     # return multiple values separated by comma
     return ..........................
 
