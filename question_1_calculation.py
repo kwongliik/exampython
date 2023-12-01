bc23bf38e95bf270c5e761e6a013988ef1a7db1e
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-12-01 18:06:08 +0800

    Update question_1_calculation.py refinement on question

--- question_1_calculation.py
+++ question_1_calculation.py
@@ -1,9 +1,16 @@
+# The following code contains errors.
+# You need to do corrections on the code.
+# Try to do corrections on online-python.com OR IDLE PYTHON before you commit the changes.
+
+# The output of the program is as follows:
+# (150, 6)
+
 def calculation(a, b):
-    multiplication = a ....... b
-    division = a ...... b
+    multiplication = a + b
+    division = a - b
     # return multiple values separated by comma
-    return ..........................
+    return a, b
 
 # get result in tuple format
-result = ........................ # call function calculation() for 30 and 5
-print(result)
+result = calculations(30, 5)
+print(result) # Don't change this line code
