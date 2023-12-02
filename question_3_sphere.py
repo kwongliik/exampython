b368dba5392994be972e3f79f7c42db6e0b5921b
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-12-02 22:10:10 +0800

    Update question_3_sphere.py question refinement

--- question_3_sphere.py
+++ question_3_sphere.py
@@ -1,13 +1,19 @@
+# This program is used to calculate the volume of sphere.
+# You need to fill in the blanks to complete the code.
+# Upon completion, you need to test run the program to give you correct output
+
 pi = 3.142
 
+# kira_sfera() is a function that return the volume of sphere
 def kira_sfera(jejari):
-    isipadu_sfera = 4/3 * pi * jejari ** 3
-    return isipadu_sfera
+    isipadu_sfera = 4/3 * ........ * ........... ** ......
+    return ...................
 
 def main():
-    a = float(input("Masukkan jejari: "))
-    isipadu_sfera = kira_sfera(a)
-    print(f"Isipadu sfera = {isipadu_sfera:.2f}")
+    a = float(input("Masukkan jejari: ")) # Don't change the code!
+    isipadu_sfera = .................. # Call the function to calculate the volume of sphere
+    print(f"Isipadu sfera = {isipadu_sfera:.2f}") # Don't change the code!
 
+# Don't change the code below!
 if __name__ == "__main__":
     main()
