d0e989be331e1905b906a2682ed7d3fcad4dac1e
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-12-03 06:18:56 +0800

    Delete test_question_5_compound_interest.py

--- test_question_5_compound_interest.py
+++
@@ -1,7 +0,0 @@
-# test_compound_interest.py
-from question_5_compound_interest import compound_interest
-
-def test_compound_interest():
-    assert compound_interest(1000, 5, 2, 1) == 1102.50
-    assert compound_interest(1500, 3.5, 3, 4) == 1665.3051756777352
-    assert compound_interest(2000, 7, 5, 12) == 2835.2505192279805
