abcf3ae96cd8440b283d75f51a4ebbbadd0f7589
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-12-02 22:27:46 +0800

    Update question_4_max.py question refinement

--- question_4_max.py
+++ question_4_max.py
@@ -1,10 +1,15 @@
+# This program will print out the maximum number among three numbers.
+# Complete the code below.
+# Upon completion, you should test run your program to get the desired output.
+
+# max_value() is a function that will return maximum number
 def max_value(a, b, c):
-    if a > b and a > c:
+    if a > b and .........:
         return a
-    elif b > a and b > c:
+    ....... b > a ......... b > c: 
         return b
     else:
-        return c
+        return ........
 
-max = max_value(30.34, 22.23, 18.89)
-print(f"maximum is {max}")
+max = ..........(30.34, 22.23, 18.89) # Call the function to return the maximum number 
+print(f"maximum is {max}") # it should output "maximum is 30.34"
