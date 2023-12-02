656ccbb3a84ea800039d4ab07377579c1ae51552
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-12-02 22:45:05 +0800

    Update question_5_compound_interest.py question refinement

--- question_5_compound_interest.py
+++ question_5_compound_interest.py
@@ -1,15 +1,26 @@
+# This program is used to calculate compound interest.
+# User can input 
+# (a) principal, 
+# (b) interest rate, 
+# (c) time in years, and 
+# (d) number of periods the interest is compounded per year (n)
+# You need to complete the code below.
+# Upon completion, you should test run the code to give you correct output.
+
+# compound_interest() is a function that will return the result of calculation
 def compound_interest(principal, rate, time, n):
-    result = principal * (pow(1 + rate / (100*n), n*time))
-    return result
+    result = principal * (.....(1 + ..... / (.......), ........)) # Use built-in function "pow()" to complete the formula
+    return result 
 
 def calculate_and_print_interest():
-    p = float(input("Enter the principal amount: "))
-    r = float(input("Enter the interest rate: "))
-    t = float(input("Enter the time in years: "))
-    n = float(input("Enter the number of periods the interest is compounded per year: "))
+    p = float(input("Enter the principal amount: ")) # Don't change the code
+    r = float(input("Enter the interest rate: ")) # Don't change the code
+    t = float(input("Enter the time in years: ")) # Don't change the code
+    n = float(input("Enter the number of periods the interest is compounded per year: ")) # Don't change the code
 
-    interest = compound_interest(p, r, t, n)
-    print("Compound interest is %.2f" % interest)
+    interest = ...............(p, r, t, n) # Call the function to calculate compound interest
+    print("Compound interest is %.2f" % interest) # Don't change the code
 
+# Don't change the code below!
 if __name__ == "__main__":
     calculate_and_print_interest()
