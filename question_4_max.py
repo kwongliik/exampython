1c7540ec31bbd60fad48027bf1c7b1abf10d9e83
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-12-03 06:36:16 +0800

    Update question_4_max.py question refinement

--- question_4_max.py
+++ question_4_max.py
@@ -1,12 +1,12 @@
 def max_value(a, b, c, d):
     if a > b and a > c and a > d:
         return a
-    elif b > a and b > c and b > d:
+    elif b > a and b > c ...... b > d:
         return b
-    elif c > a and c > b and c > d:
+    ....... c > a and c > b and c > d:
         return c
     else:
-        return d
+        return ......
 
-max = max_value(30.34, 22.23, 48.89, 27.49)
+max = ..........(30.34, 22.23, 48.89, 27.49)
 print(f"Maximum is {max}")
