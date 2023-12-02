ba4ada2c0ff3e8e37c3af8e6ef491cdaec8ac9a2
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-12-02 21:55:51 +0800

    Update question_1_calculation.py question refinement

--- question_1_calculation.py
+++ question_1_calculation.py
@@ -2,15 +2,10 @@
 # You need to do corrections on the code.
 # Try to do corrections on online-python.com OR IDLE PYTHON before you commit the changes.
 
-# The output of the program is as follows:
-# (150, 6.0)
-
 def calculation(a, b):
     multiplication = a + b
     division = a - b
-    # return multiple values separated by comma
-    return a, b
+    return a, b  # return multiple values separated by comma
 
-# get result in tuple format
-result = calculations(30, 5)
-print(result) # Don't change this line code
+result = calculations(30, 5) # Call the function to get the result
+print(result) # Don't change the code. It shows the output as "(150, 6.0)"
