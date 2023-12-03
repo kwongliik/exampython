0bcb4eadbff6b92d1d9726b619ad118b10627858
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-12-03 12:32:18 +0800

    Update question_1_calculation.py question refinement

--- question_1_calculation.py
+++ question_1_calculation.py
@@ -6,7 +6,7 @@ def calculation(a, b): # Don't change the code
     subtraction = a * b
     multiplication = a + b
     division = a - b
-    return a, b, division  # return multiple values separated by comma
+    return a, b, division  # return 3 values separated by comma
 
-result = calculations(30, 5) # Call the function to get the result
+result = calculations(30, 5) # Call the function to get the result. Use 30 and 5 as argument.
 print(result) # Don't change the code. It shows the output as "(25, 150, 6.0)"
