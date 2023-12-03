87be24e158dc3e633fb0f486e122748112c1854a
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-12-03 17:21:02 +0800

    question 5 refinement

--- question_5_compound_interest.py
+++ question_5_compound_interest.py
@@ -12,15 +12,15 @@ def compound_interest(principal, rate, time, n):
     result = principal * (.....(1 + ..... / (100 * .....), ........)) # Use built-in function "pow()" to complete the formula
     return result 
 
-def calculate_and_print_interest():
+def calculate_and_print_matured_value():
     p = float(input("Enter the principal amount: ")) # Don't change the code
     r = float(input("Enter the interest rate: ")) # Don't change the code
     t = float(input("Enter the time in years: ")) # Don't change the code
     n = float(input("Enter the number of periods the interest is compounded per year: ")) # Don't change the code
 
-    interest = ...............(p, r, t, n) # Call the function to calculate compound interest
-    print("Compound interest is %.2f" % interest) # Don't change the code
+    matured_value = ...............(p, r, t, n) # Call the function to calculate matured value
+    print("Matured value is %.2f" % matured_value) # Don't change the code
 
 # Don't change the code below!
 if __name__ == "__main__":
-    calculate_and_print_interest()
+    calculate_and_print_matured_value()
