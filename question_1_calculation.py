f0a5e85b043f8d18b840fd1b142228c42a4b67dd
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-12-03 12:24:33 +0800

    Update question_1_calculation.py question refinement

--- question_1_calculation.py
+++ question_1_calculation.py
@@ -1,11 +1,12 @@
 # The following code contains errors.
-# You need to do corrections on the code.
+# You need to do 5 corrections on the code.
 # Try to do corrections on online-python.com OR IDLE PYTHON before you commit the changes.
 
-def calculation(a, b):
+def calculation(a, b): # Don't change the code
+    subtraction = a * b
     multiplication = a + b
     division = a - b
-    return a, b  # return multiple values separated by comma
+    return a, b, division  # return multiple values separated by comma
 
 result = calculations(30, 5) # Call the function to get the result
-print(result) # Don't change the code. It shows the output as "(150, 6.0)"
+print(result) # Don't change the code. It shows the output as "(25, 150, 6.0)"
